5a9ddc8843747a2a0fdf32377bbed8fe632d0828
hainl2000 <haingluong2000@gmail.com>
2022-04-29 17:15:41 +0700

    add namespace to PostTransormers

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Comment;
 use App\Models\Post;
 
-use App\Transformers;
+use App\Transformers\PostTransformers;
 use Illuminate\Http\Request;
 use Spatie\Fractal\Fractal;
 
 class PostController extends Controller
 {
+
+
     public function index()
     {
         $posts = Post::all();
-        $data = \fractal()->collection($posts)->transformWith(new \PostTransormers())->parseIncludes('user')->toArray();
+
+        $data = \fractal()->collection($posts)->transformWith(new PostTransformers())->parseIncludes('user')->toArray();
         return \response()->json([
             'data' => $data
         ],200);
